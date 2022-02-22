2e53f66dc66c1cd846e0d7b4b1209f82465dfb22
mecene <mecenephrygien@gmail.com>
2022-02-22 13:21:01 +0300

    permission controller termine

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -2,18 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use DB;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
 
 class PermissionController extends Controller
 {
+    /**
+     * create a new instance of the class
+     *
+     * @return void
+     */
+    function __construct()
+    {
+         $this->middleware('permission:permission-list|permission-create|permission-edit|permission-delete', ['only' => ['index','store']]);
+         $this->middleware('permission:permission-create', ['only' => ['create','store']]);
+         $this->middleware('permission:permission-edit', ['only' => ['edit','update']]);
+         $this->middleware('permission:permission-delete', ['only' => ['destroy']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $data = Permission::orderBy('id','DESC')->paginate(5);
+
+        return view('permissions.index', compact('data'));
     }
 
     /**
@@ -23,7 +40,7 @@ class PermissionController extends Controller
      */
     public function create()
     {
-        //
+        return view('permissions.create');
     }
 
     /**
@@ -34,7 +51,14 @@ class PermissionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'name' => 'required|unique:permissions,name',
+        ]);
+    
+        Permission::create(['name' => $request->input('name')]);
+    
+        return redirect()->route('permissions.index')
+            ->with('success', 'Permission created successfully.');
     }
 
     /**
@@ -45,7 +69,9 @@ class PermissionController extends Controller
      */
     public function show($id)
     {
-        //
+        $permission = Permission::find($id);
+    
+        return view('permissions.show', compact('permission'));
     }
 
     /**
@@ -56,7 +82,9 @@ class PermissionController extends Controller
      */
     public function edit($id)
     {
-        //
+        $permission = Permission::find($id);
+    
+        return view('permissions.edit', compact('permission'));
     }
 
     /**
@@ -68,7 +96,16 @@ class PermissionController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'name' => 'required'
+        ]);
+    
+        $permission = Permission::find($id);
+        $permission->name = $request->input('name');
+        $permission->save();
+        
+        return redirect()->route('permissions.index')
+            ->with('success', 'Permission updated successfully.');
     }
 
     /**
@@ -79,6 +116,9 @@ class PermissionController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Permission::find($id)->delete();
+        
+        return redirect()->route('permissions.index')
+            ->with('success', 'Permission deleted successfully');
     }
 }
